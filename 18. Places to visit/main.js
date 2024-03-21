6dfeb3ed4fac7a1b25a6de8dc919112e25b63633
floydcal <160587879+floydcal@users.noreply.github.com>
2024-03-21 18:27:07 +0500

    places to visit

---
+++ 18. Places to visit/main.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let favPlaces = ["Swat", "Fiji", "Switzerland", "Rome", "USA", "Canada"];
+for (let i = 0; i < favPlaces.length; i++) {
+    console.log(favPlaces[i]);
+}
+let sortedArray = favPlaces.sort();
+console.log(sortedArray);
+for (let i = 0; i < favPlaces.length; i++) {
+    console.log(favPlaces[i]);
+}
+let reversedArray = sortedArray.reverse();
+console.log(reversedArray);
+for (let i = 0; i < favPlaces.length; i++) {
+    console.log(favPlaces[i]);
+}
---
+++ 18. Places to visit/main.ts
@@ -0,0 +1,17 @@
+let favPlaces: string[]=["Swat", "Fiji", "Switzerland", "Rome","USA", "Canada"];
+for (let i=0; i<favPlaces.length; i++){
+    console.log(favPlaces[i]);
+    }
+
+let sortedArray=favPlaces.sort();
+console.log (sortedArray);
+
+for (let i=0; i<favPlaces.length; i++){
+    console.log(favPlaces[i]);
+    }
+let reversedArray=sortedArray.reverse();
+console.log(reversedArray);
+
+for (let i=0; i<favPlaces.length; i++){
+    console.log(favPlaces[i]);
+    }
