16669abbd710f6431df64f8fc09af58eabc67828
floydcal <160587879+floydcal@users.noreply.github.com>
2024-03-26 19:25:29 +0500

    no users

---
+++ 31. No users/index.ts
@@ -0,0 +1,23 @@
+let userName: string[] = ["Floyd", "Waqas", "Shumail"];
+
+if (userName.length > 0) {
+    for (let username of userName) {
+        console.log(`Hello ${username}, thank you for logging in!`);
+    }
+    let adminUser: string = "Arif";
+    console.log(`Welcome ${adminUser}, would you like a status report`);
+} else {
+    console.log("We need to find some users!");
+}
+
+userName = [];
+
+if (userName.length > 0) {
+    for (let username of userName) {
+        console.log(`Hello ${username}, thank you for logging in!`);
+    }
+    let adminUser: string = "Arif";
+    console.log(`Welcome ${adminUser}, would you like a status report`);
+} else {
+    console.log("We need to find some users!");
+}
