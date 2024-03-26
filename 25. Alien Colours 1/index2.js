7ef231cce81dd2de4aeabd936084f3adff83c37f
floydcal <160587879+floydcal@users.noreply.github.com>
2024-03-26 19:05:47 +0500

    alien color1

---
+++ 25. Alien Colours 1/index2.js
@@ -0,0 +1,5 @@
+let alien_color = 'green';
+if (alien_color === 'red') {
+    console.log("The player just earned 5 points!");
+}
+export {};
---
+++ 25. Alien Colours 1/index2.ts
@@ -0,0 +1,5 @@
+let alien_color: string = 'green';
+
+if (alien_color === 'red') {
+    console.log("The player just earned 5 points!");
+}
