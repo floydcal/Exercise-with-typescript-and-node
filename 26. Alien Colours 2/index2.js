f23aac51e3a97bc4191ece6cfbd5c3ad9143c499
floydcal <160587879+floydcal@users.noreply.github.com>
2024-03-26 19:12:27 +0500

    alien colors2

---
+++ 26. Alien Colours 2/index2.js
@@ -0,0 +1,8 @@
+let alien_color = 'red';
+if (alien_color === 'green') {
+    console.log("The player just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("The player just earned 10 points!");
+}
+export {};
