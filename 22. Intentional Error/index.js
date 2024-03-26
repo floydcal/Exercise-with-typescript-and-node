6c28d00b93c10b23edd86028173dee78c911cabd
floydcal <160587879+floydcal@users.noreply.github.com>
2024-03-26 18:28:58 +0500

    interntional error

---
+++ 22. Intentional Error/index.js
@@ -0,0 +1,4 @@
+let favPlaces = ["Mountains", "Rivers", "Valleys", "Snow"];
+console.log(`${favPlaces[4]}`);
+console.log(`${favPlaces[3]}`);
+export {};
---
+++ 22. Intentional Error/index.ts
@@ -0,0 +1,7 @@
+let favPlaces: string[]=["Mountains","Rivers", "Valleys", "Snow"];
+
+
+console.log(`${favPlaces[4]}`); 
+
+
+console.log(`${favPlaces[3]}`); 
