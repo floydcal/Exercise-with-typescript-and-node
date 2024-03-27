727114b5c632f76bcf708d350e8cdf16921279ba
floydcal <160587879+floydcal@users.noreply.github.com>
2024-03-27 18:53:44 +0500

    magicians

---
+++ 41. Magicians/index.js
@@ -0,0 +1,8 @@
+function show_magicians(magicians) {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+let magicians = ["David Copperfield", "Harry Houdini", "Penn Jillette", "Teller"];
+show_magicians(magicians);
+export {};
---
+++ 41. Magicians/index.ts
@@ -0,0 +1,10 @@
+function show_magicians(magicians: string[]): void {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+
+let magicians: string[] = ["David Copperfield", "Harry Houdini", "Penn Jillette", "Teller"];
+
+
+show_magicians(magicians);
