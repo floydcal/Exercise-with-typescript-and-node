7162a3a273e3c45b85717d240ce4377786231ad2
floydcal <160587879+floydcal@users.noreply.github.com>
2024-03-27 18:19:51 +0500

    Pizza

--- 34. Pizza/index.js
+++ 34. Pizza/index.js
@@ -2,5 +2,8 @@ let favoritePizzas = ["Pepperoni", "Fajita", "BBQ Chicken"];
 for (let pizza of favoritePizzas) {
     console.log(`I like ${pizza} pizza.`);
 }
+console.log("Pizza is the best!");
+console.log("It's always a good time for pizza!");
+console.log("My children love Pizza!");
 console.log("I really love pizza!");
 export {};
--- 34. Pizza/index.ts
+++ 34. Pizza/index.ts
@@ -4,4 +4,7 @@ let favoritePizzas: string[] = ["Pepperoni", "Fajita", "BBQ Chicken"];
 for (let pizza of favoritePizzas) {
     console.log(`I like ${pizza} pizza.`);
 }
+console.log("Pizza is the best!");
+console.log("It's always a good time for pizza!");
+console.log("My children love Pizza!");
 console.log("I really love pizza!");
